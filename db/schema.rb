87349b3695171dc6119eea177f2b23c84dfb2c3e
david hightower <w.david.hightower@gmail.com>
2017-11-11 01:03:56 +0000

    user partial

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171110235756) do
+ActiveRecord::Schema.define(version: 20171111001930) do
 
   create_table "incident_reports", force: :cascade do |t|
-    t.date "occurred_on"
     t.string "category"
-    t.string "address"
+    t.string "street_number"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.string "country"
+    t.decimal "lattitude"
+    t.decimal "longitude"
+    t.date "occurred_on"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
