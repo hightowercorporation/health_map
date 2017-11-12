50a1cb281ca8fc866ef581e4bd4227d92cb3918b
david hightower <w.david.hightower@gmail.com>
2017-11-12 23:46:03 +0000

    nav bar started

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :incident_reports
   get 'home/index'
 	root 'home#index'
-	
+get 'users' => 'static#hello'
   root to: 'visitors#index'
   devise_for :users
   resources :users
